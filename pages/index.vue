450f2e74a811dd897fc4b7efd514cd96280bdfe0
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-02-06 09:30:28 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>1</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  head() {
+    return {
+      title: 'Flycket',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'My custom description',
+        },
+
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'Flycket',
+        },
+      ],
+    };
+  },
+};
+</script>
